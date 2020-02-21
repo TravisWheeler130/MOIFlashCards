2c4daa2a8bc0b47ec352f73663251d73acf22c47
Travis Wheeler <travis.wheeler130@yahoo.com>
2020-02-21 12:48:17 -0700

    project start

---
+++ MOIFlashcards/MOIFlashcards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MOIFlashcards
+//
+//  Created by Travis Wheeler on 2/21/20.
+//  Copyright © 2020 Travis Wheeler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
