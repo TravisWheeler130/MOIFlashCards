641c176490574673c15dda50f0426d14c1b6fede
Travis Wheeler <travis.wheeler130@yahoo.com>
2020-03-05 21:42:30 -0700

    created core data model and convenience model.

---
+++ MOIFlashcards/MOIFlashcards/Model/CoreDataStack.swift
@@ -0,0 +1,9 @@
+//
+//  CoreDataStack.swift
+//  MOIFlashcards
+//
+//  Created by Travis Wheeler on 3/5/20.
+//  Copyright © 2020 Travis Wheeler. All rights reserved.
+//
+
+import Foundation
---
+++ MOIFlashcards/MOIFlashcards/Model/Flashcards+Convenience.swift
@@ -0,0 +1,9 @@
+//
+//  Flashcards+Convenience.swift
+//  MOIFlashcards
+//
+//  Created by Travis Wheeler on 3/5/20.
+//  Copyright © 2020 Travis Wheeler. All rights reserved.
+//
+
+import Foundation
